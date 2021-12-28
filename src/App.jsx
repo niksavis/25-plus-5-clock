18f97de779ced84d26fd06c8e661674cf5ea18e6
Niksa Visic <niksavis@live.com>
2021-12-28 16:14:45 +0100

    time buttons functionality

--- src/App.jsx
+++ src/App.jsx
@@ -13,21 +13,113 @@ class App extends React.Component {
   state = {
     breakCount: 5,
     sessionCount: 25,
+    clockCount: 25 * 60,
+    currentTimer: 'Session',
+    isPlaying: false,
+    loop: undefined,
+  };
+
+  constructor(props) {
+    super(props);
+    this.loop = undefined;
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.loop);
+  }
+
+  handlePlayPause = () => {
+    const { isPlaying } = this.state;
+
+    if (isPlaying) {
+      clearInterval(this.loop);
+      this.setState({ isPlaying: false });
+    } else {
+      this.setState({ isPlaying: true });
+      this.loop = setInterval(() => {
+        const { clockCount, currentTimer, breakCount, sessionCount } =
+          this.state;
+
+        if (clockCount === 0) {
+          this.setState({
+            currentTimer: currentTimer === 'Session' ? 'Break' : 'Session',
+            clockCount:
+              currentTimer === 'Session' ? breakCount * 60 : sessionCount * 60,
+          });
+        } else {
+          this.setState({ clockCount: clockCount - 1 });
+        }
+      }, 1000);
+    }
+  };
+
+  handleReset = () => {
+    this.setState({
+      breakCount: 5,
+      sessionCount: 25,
+      clockCount: 25 * 60,
+      currentTimer: 'Session',
+      isPlaying: false,
+    });
+
+    clearInterval(this.loop);
+  };
+
+  convertToTime = (count) =>
+    count < 3600
+      ? new Date(count * 1000).toISOString().substring(14, 19)
+      : new Date(count * 1000).toISOString().substring(11, 19);
+
+  handleBreakDecrease = () => {
+    const { breakCount } = this.state;
+    if (breakCount > 0) {
+      this.setState({
+        breakCount: breakCount - 1,
+      });
+    }
+  };
+
+  handleBreakIncrease = () => {
+    const { breakCount } = this.state;
+    if (breakCount < 1440) {
+      this.setState({
+        breakCount: breakCount + 1,
+      });
+    }
+  };
+
+  handleSessionDecrease = () => {
+    const { sessionCount } = this.state;
+    if (sessionCount > 0) {
+      this.setState({
+        sessionCount: sessionCount - 1,
+      });
+    }
+  };
+
+  handleSessionIncrease = () => {
+    const { sessionCount } = this.state;
+    if (sessionCount < 1440) {
+      this.setState({
+        sessionCount: sessionCount + 1,
+      });
+    }
   };
 
   render() {
-    const { breakCount, sessionCount } = this.state;
+    const { breakCount, sessionCount, clockCount, currentTimer, isPlaying } =
+      this.state;
 
     const breakProps = {
       title: 'Break Length',
-      count: 5,
+      count: breakCount,
       handleDecrease: this.handleBreakDecrease,
       handleIncrease: this.handleBreakIncrease,
     };
 
     const sessionProps = {
       title: 'Session Length',
-      count: 25,
+      count: sessionCount,
       handleDecrease: this.handleSessionDecrease,
       handleIncrease: this.handleSessionIncrease,
     };
@@ -39,12 +131,11 @@ class App extends React.Component {
           <SetTimer {...sessionProps} />
         </div>
         <div className="clock-container">
-          <h1>Session</h1>
-          <span>25:00</span>
+          <h1>{currentTimer}</h1>
+          <span>{this.convertToTime(clockCount)}</span>
           <div className="flex">
             <button onClick={this.handlePlayPause}>
-              <FontAwesomeIcon icon={faPlay} />
-              <FontAwesomeIcon icon={faPause} />
+              <FontAwesomeIcon icon={isPlaying ? faPause : faPlay} />
             </button>
             <button onClick={this.handleReset}>
               <FontAwesomeIcon icon={faSync} />
